b88482835499b7cc160ef842cb06684f1876112e
Mahdietabatabaiee <mahd.tabatabaee@gmail.com>
2023-04-21 18:58:04 +0430

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace math2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter x : ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter y : ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(clculator(x, y));
+
+        }
+        private static double clculator(double x, double y)
+
+        {
+            return Math.Sqrt(Math.Abs(x)) + Math.Pow(y, 2);
+        }
+    }
+}
